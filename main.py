050606b75f09ddc75698a4c1f659126261d9b311
Silver <silverstacksoftware@gmail.com>
2024-08-31 16:44:20 +0200

    Working docker file

--- main.py
+++ main.py
@@ -37,13 +37,38 @@ async def sentinel_corsair(request: ImageRequest):
         parts_detections = result.get('parts_detections')
 
         response_data = {
-            "image_base64": image_base64,
-            "parts": parts_detections
+            'parts': parts_detections
         }
 
-        # Convert the dictionary to a JSON string
-        json_response = json.dumps(response_data)
+        return response_data
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error processing image: {str(e)}")
 
-        return json_response
+@app.post("/sentinel-corsair-image")
+async def sentinel_corsair(request: ImageRequest):
+    try:
+        response = requests.get(request.url)
+        if response.status_code == 200:
+
+            image_data = response.content
+            base64_encoded = base64.b64encode(image_data).decode('utf-8')
+
+        else:
+            raise Exception(f"Failed to fetch image. Status code: {response.status_code}")
+
+        result = damage_detector.predict_and_draw(base64_encoded)
+
+        if result is None:
+            raise HTTPException(status_code=400, detail="No damage detected in the image.")
+
+        image_base64 = result.get('image')
+        parts_detections = result.get('parts_detections')
+
+        response_data = {
+            'image_base64': image_base64,
+            'parts': parts_detections
+        }
+
+        return response_data
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error processing image: {str(e)}")
